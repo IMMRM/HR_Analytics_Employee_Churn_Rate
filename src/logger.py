803254bf6edb8afe9d275056303a94bb1d1272b1
Immrm <mrmirdha@outlook.com>
2025-02-17 22:30:26 +0530

    Custom logging implemented

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+#### This is the logger file
+import os
+import sys
+import logging
+from datetime import datetime
+
+logging_str="[%(asctime)s : %(levelname)s: %(module)s: %(message)s]"
+log_dir="logs"
+
+log_filepath=os.path.join(log_dir,f"log_{datetime.now().strftime('%Y-%m-%d_%H')}.log")
+
+os.makedirs(log_dir,exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath), #this will write the log into the log file
+        logging.StreamHandler(sys.stdout) #this will print the message on console
+    ]
+)
+
+logger=logging.getLogger("CustomLogger")
